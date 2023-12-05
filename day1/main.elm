c792d27c03e19ae7eabda0a93286fa2cf41d1b45
Esten Patrick <esten.patrick@amount.com>
2023-12-04 22:44:55 -0600

    part 1 correct

--- day1/main.elm
+++ day1/main.elm
@@ -1,9 +1,34 @@
+module Main exposing (..)
+
 import Html exposing (text)
 
 import Data exposing (linesIn)
 
 calibrate : String -> Int
-calibrate lines = 45
+calibrate lines =
+  lines
+    |> String.words -- `lines` here is more semantic, but results in extra whitespace to clean up
+    |> List.map toDoubleDigInt
+    |> List.sum
+
+
+toDoubleDigInt : String -> Int
+toDoubleDigInt line =
+
+  let
+    filtered =
+      line
+        |> String.toList
+        |> List.map String.fromChar
+        |> (List.filterMap String.toInt)
+
+
+    left = filtered |> List.head |> Maybe.withDefault 0
+    right = filtered |> List.reverse |> List.head |> Maybe.withDefault 0
+
+    doubleDigit = (String.fromInt left) ++ (String.fromInt right) |> String.toInt |> Maybe.withDefault 0
+  in
+    doubleDigit
 
 
 
@@ -12,6 +37,3 @@ main =
     |> calibrate
     |> String.fromInt
     |> text
-
-
--- CONSTANTS / METADATA
