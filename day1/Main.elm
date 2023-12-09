dd811d71af75b06cbe6a98e77a092cfb5a1a9bfd
estenp <estenpatrick@gmail.com>
2023-12-08 23:51:34 -0600

    some cleanup and refactor

--- day1/Main.elm
+++ day1/Main.elm
@@ -52,36 +52,37 @@ evaluateLine line =
            actual, or a word representing one.
         -}
         toOuterNumber : Direction -> Char -> String -> String
-        toOuterNumber direction char acc =
-            -- if acc is a digit, we've found so just loop out while retaining it
-            if acc |> String.toList |> List.head |> Maybe.withDefault '_' |> Char.isDigit then
-                acc
-                -- if the next char is an actual digit, we've just found so return as accumulator
+        toOuterNumber direction nextChar acc =
 
-            else if Char.isDigit char then
-                String.fromChar char
-                -- otherwise, see if new char creates a number word when added to accumulator string
-
-            else
-                let
-                    newAcc =
-                        if direction == Left then
-                            acc ++ String.fromChar char
-
+            case String.toInt (String.left 1 acc) of
+                    -- if first char of acc can be converted to Int, we've found digit so loop out
+                    Just _ ->
+                        acc
+                    -- otherwise take next char and proceed
+                    Nothing ->
+                        if Char.isDigit nextChar then
+                            String.fromChar nextChar
                         else
-                            String.fromChar char ++ acc
+                            -- otherwise, see if new char creates a number word when added to accumulator string
+                            let
+                                newAcc =
+                                    case direction of
+                                        Left ->
+                                            acc ++ String.fromChar nextChar
+                                        Right ->
+                                            String.fromChar nextChar ++ acc
 
-                    containsNumberWord =
-                        Dict.keys numDict |> List.filter (\s -> String.contains s newAcc)
-                in
-                case containsNumberWord of
-                    -- no word found, keep iterating
-                    [] ->
-                        newAcc
+                                containsNumberWord =
+                                    Dict.keys numDict |> List.filter (\s -> String.contains s newAcc)
+                            in
+                            case containsNumberWord of
+                                -- no word found, keep iterating
+                                [] ->
+                                    newAcc
 
-                    -- word found, lookup digit value and return as accumulator
-                    x :: _ ->
-                        Dict.get x numDict |> Maybe.withDefault newAcc
+                                -- word found, lookup digit value and return as accumulator
+                                x :: _ ->
+                                    Dict.get x numDict |> Maybe.withDefault newAcc
 
         l =
             lineAsList
