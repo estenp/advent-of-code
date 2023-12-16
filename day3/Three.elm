fbb526af946c22eb91c5ac59143960924deee75d
estenp <estenpatrick@gmail.com>
2023-12-15 22:14:37 -0600

    realized I did it wrong because i was only looking for single digit numbers

--- day3/Three.elm
+++ day3/Three.elm
@@ -1,11 +1,12 @@
 module Three exposing (..)
 
-import Data exposing (day3Sample)
+import Array
+import Data exposing (day3Sample, day)
 import Dict
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import List.Extra as ListExtra
 import Utils exposing (trimLines)
-import Array
 
 
 main =
@@ -15,15 +16,13 @@ main =
                 |> String.lines
                 |> trimLines
 
-        rowCount = List.length input
-
-        (isValidInput, columnCount) =
+        isValidInput =
             case List.head input of
                 Just str ->
-                    (List.all (\l -> String.length l == String.length str) input, String.length str)
+                    List.all (\l -> String.length l == String.length str) input
 
                 Nothing ->
-                    (False, 0)
+                    False
     in
     if isValidInput then
         input
@@ -35,22 +34,80 @@ main =
         Debug.todo "Problem with validation"
 
 
--- sumPartNums : List String -> Int
+sumPartNums : List String -> Int
 sumPartNums lines =
     let
+        schematic : Dict.Dict Int String
+        schematic =
+            Dict.fromList (List.indexedMap Tuple.pair lines)
 
-        schematic : Dict.Dict Int (Array.Array Char)
-        schematic = Dict.fromList (List.indexedMap Tuple.pair lines) |> Dict.map (\_ a -> Array.fromList (String.toList a))
+        -- fold over dictionary so index of row is available
+        partNumList =
+            Dict.foldl (\k v a -> a ++ rowToPartnums k v) [] schematic
 
-        validPartNum (char, row, col) =
+        -- in reducer, fold each value into list of part nums
+        rowToPartnums : Int -> String -> List Char
+        rowToPartnums rowIndex rowStr =
+            rowStr
+                |> String.toList
+                |> ListExtra.indexedFoldl
+                    (\colIndex char acc ->
+                        if isValidPartNum rowIndex colIndex char then
+                            char :: acc
+
+                        else
+                            acc
+                    )
+                    []
+                |> Debug.log "check"
+
+        -- check a char for adjacent symbols
+        isValidPartNum : Int -> Int -> Char -> Bool
+        isValidPartNum rowIndex colIndex char =
             let
-                prevRow = Dict.get (row - 1)
-                nextRow = Dict.get (row + 1)
+                currRow =
+                    Dict.get rowIndex schematic
 
+                prevRow =
+                    Dict.get (rowIndex - 1) schematic
 
+                nextRow =
+                    Dict.get (rowIndex + 1) schematic
+
+                isValidChar c =
+                    not (Char.isAlphaNum c) && (c /= '.')
+
+                adjacent row =
+                    case row of
+                        Just lineStr ->
+                            let
+                                checkByIndex i =
+                                    case String.toList lineStr |> Array.fromList |> Array.get i of
+                                        Just c ->
+                                            isValidChar c
+
+                                        Nothing ->
+                                            False
+
+                                left =
+                                    checkByIndex (colIndex - 1)
+
+                                mid =
+                                    checkByIndex colIndex
+
+                                right =
+                                    checkByIndex (colIndex + 1)
+                            in
+                            left || mid || right
+
+                        Nothing ->
+                            False
+
+                isAdjacentToSymbol =
+                    adjacent prevRow || adjacent nextRow || adjacent currRow
             in
-                char
-
-
+            Char.isDigit char && isAdjacentToSymbol
     in
-        schematic
+    partNumList
+        |> List.map (String.fromChar >> String.toInt >> Maybe.withDefault 0)
+        |> List.sum
