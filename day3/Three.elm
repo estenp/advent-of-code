4058fab51b6e04cb41bb0669f190fb40474b0db1
estenp <estenpatrick@gmail.com>
2023-12-17 16:31:17 -0600

    total mess

--- day3/Three.elm
+++ day3/Three.elm
@@ -1,7 +1,7 @@
 module Three exposing (..)
 
 import Array
-import Data exposing (day3Sample, day)
+import Data exposing (day3Sample)
 import Dict
 import Html exposing (..)
 import Html.Attributes exposing (..)
@@ -41,29 +41,48 @@ sumPartNums lines =
         schematic =
             Dict.fromList (List.indexedMap Tuple.pair lines)
 
+        -- _ =
+        --     Dict.map
+        --         (\k v ->
+        --             String.toList v
+        --                 |> List.map
+        --                     (\c ->
+        --                         if Char.isDigit c then
+        --                             c
+
+        --                         else
+        --                             ' '
+        --                     )
+        --                 -- |> String.fromList
+        --                 -- |> String.words
+        --         )
+        --         schematic
+        --         |> Debug.log "filter nums"
+
         -- fold over dictionary so index of row is available
         partNumList =
             Dict.foldl (\k v a -> a ++ rowToPartnums k v) [] schematic
 
         -- in reducer, fold each value into list of part nums
-        rowToPartnums : Int -> String -> List Char
+        rowToPartnums : Int -> String -> List Int
         rowToPartnums rowIndex rowStr =
             rowStr
                 |> String.toList
                 |> ListExtra.indexedFoldl
                     (\colIndex char acc ->
-                        if isValidPartNum rowIndex colIndex char then
-                            char :: acc
+                        case maybeValidPartNum rowIndex colIndex char of
+                            Just partNum ->
+                                partNum :: acc
 
-                        else
-                            acc
+                            Nothing ->
+                                acc
                     )
                     []
                 |> Debug.log "check"
 
         -- check a char for adjacent symbols
-        isValidPartNum : Int -> Int -> Char -> Bool
-        isValidPartNum rowIndex colIndex char =
+        maybeValidPartNum : Int -> Int -> Char -> Maybe Int
+        maybeValidPartNum rowIndex colIndex char =
             let
                 currRow =
                     Dict.get rowIndex schematic
@@ -105,9 +124,48 @@ sumPartNums lines =
 
                 isAdjacentToSymbol =
                     adjacent prevRow || adjacent nextRow || adjacent currRow
+
+                rowToIndexedList : List ( Int, Char )
+                rowToIndexedList = case currRow of
+                        Just rowString ->
+                            rowString
+                                |> String.toList
+                                |> Array.fromList
+                                |> Array.toIndexedList
+                                |> Debug.log "indexed list"
+                                -- |> Debug.todo "asdf"
+                        Nothing ->
+                            0
+
+                gatherPartNum li ri acc =
+                    if (li == Nothing) && (ri == Nothing) then
+                        acc
+                    else
+                        let
+                            -- check l and r index for numbers
+                            -- if found add to acc
+
+                            -- this seems like a Maybe.map thing or something?
+                            left = case (ListExtra.find (\t -> (Tuple.first t) == li) rowToIndexedList) of
+                                Just t ->
+                                    (Tuple.first t) - 1
+                                Nothing ->
+                                    Nothing
+
+
+                            -- base case
+                            -- if li has no number and ri has no number stop and return acc
+                        in
+                            gatherPartNum (li - 1) (ri + 1) acc
+
+
+
             in
-            Char.isDigit char && isAdjacentToSymbol
+            if Char.isDigit char && isAdjacentToSymbol then
+                Just (gatherPartNum (colIndex - 1))
+            else
+                Nothing
     in
     partNumList
-        |> List.map (String.fromChar >> String.toInt >> Maybe.withDefault 0)
+        -- |> List.map (String.fromChar >> String.toInt >> Maybe.withDefault 0)
         |> List.sum
