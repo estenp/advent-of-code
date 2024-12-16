92afd3d3e92f9590b6d08c4b61671562ba4189cb
Esten Patrick <epatrick@uisg.com>
2024-12-15 22:43:42 -0600

    working, cleaner

--- 2024/Day1/A.elm
+++ 2024/Day1/A.elm
@@ -13,41 +13,44 @@ main =
 
 
 calcTotalDistance : String -> Int
-calcTotalDistance s =
-    let
-        asInts =
-            List.map (String.toInt >> Maybe.withDefault -1)
+calcTotalDistance =
+    String.lines
+        -- remove empty first and last lines
+        >> List.filter ((/=) "")
+        -- create list of tuples respective to lines
+        >> List.map
+            (lineToTwoList >> List.map toInt >> twoListToTuple)
+        >> List.unzip
+        -- sort each column
+        >> Tuple.mapBoth List.sort List.sort
+        >> zip
+        >> List.map (\( l, r ) -> abs (l - r))
+        >> List.sum
 
-        sortedAsInts =
-            asInts >> List.sort
 
-        pairs : List a -> List b -> List ( a, b )
-        pairs xs ys =
-            List.map2 Tuple.pair xs ys
+toInt : String -> Int
+toInt =
+    String.toInt >> Maybe.withDefault -1
 
-        ( left, right ) =
-            s
-                |> String.lines
-                |> List.filter ((/=) "")
-                |> List.map
-                    (String.split "  "
-                        >> List.map String.trim
-                        >> (\twoList ->
-                                case twoList of
-                                    [ a, b ] ->
-                                        ( a, b )
 
-                                    _ ->
-                                        ( "", "" )
-                           )
+zip : ( List a, List b ) -> List ( a, b )
+zip ( xs, ys ) =
+    List.map2 Tuple.pair xs ys
 
-                    )
-                |> List.unzip
-                |> Tuple.mapBoth sortedAsInts sortedAsInts
 
-        lowestPairs =
-            pairs left right
-    in
-    lowestPairs
-        |> List.map (\( l, r ) -> abs (l - r))
-        |> List.sum
+lineToTwoList : String -> List String
+lineToTwoList =
+    String.split "  " >> List.map String.trim
+
+
+twoListToTuple : List Int -> ( Int, Int )
+twoListToTuple list =
+    case list of
+        [ a, b ] ->
+            ( a, b )
+
+        a :: b :: _ ->
+            ( a, b )
+
+        _ ->
+            ( -1, -1 )
